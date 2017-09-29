b4ccb8049c3b3c7edfa93a5b0f2d946dbfb75a6f
hjlee <hjlee1118@naver.com>
2017-09-29 17:31:17 +0900

    .

--- sql_file/addColumn.sql
+++ sql_file/addColumn.sql
@@ -15,15 +15,39 @@ select sum(COMMUNICATION) as communication, sum(RESTAURANT) as restaurant, sum(M
 sum(SUPERMARKET) as superMarket, sum(BOOKSTORE) as bookstore, sum(MOVIE) as movie, sum(CAFETERIA) as cafeteria, sum(ONLINESHOPPING) as onlineShopping, sum(ACADEMY) as academy, sum(TRANSPORTATION) as transportation,
 sum(CAFE) as cafe, sum(CONVENIENCE) as convenience, sum(AMUSEMENTPARK) as amusementPark, sum(MEDICAL) as medical
 from SEARCH_KEYWORD 
-where card_type='credit' and age between 20 and 29;
+where card_type=`credit` and age between 20 and 29;
 
 
-select * from MEMBER where id = 'hjlee1118';
+select * from MEMBER where id = `hjlee1118`;
 
 
 update MEMBER
 set CARD1 = NULL, CARD2 = NULL, CARD3 = NULL
-where id = 'hjlee1118';
+where id = `hjlee1118`;
 
 commit;
+
+CREATE TABLE RECOMMEND_COUNT
+(
+   `NO`         INT(11) UNSIGNED           primary key auto_increment, 
+   `BRAND`      VARCHAR(200)    NULL, 
+   `CARD_TYPE`   VARCHAR(200)    NULL, 
+    `COMMUNICATION`   INT(11) UNSIGNED           NULL, 
+`RESTAURANT`   INT(11) UNSIGNED           NULL, 
+`MART`   INT(11) UNSIGNED           NULL, 
+`BEAUTY`   INT(11) UNSIGNED           NULL, 
+`GASSTATION`   INT(11) UNSIGNED           NULL, 
+`FASTFOOD`   INT(11) UNSIGNED           NULL, 
+`SUPERMARKET`   INT(11) UNSIGNED           NULL, 
+`BOOKSTORE`   INT(11) UNSIGNED           NULL, 
+`MOVIE`   INT(11) UNSIGNED           NULL, 
+`CAFETERIA`   INT(11) UNSIGNED           NULL, 
+`ONLINESHOPPING`   INT(11) UNSIGNED           NULL, 
+`ACADEMY`   INT(11) UNSIGNED           NULL, 
+`TRANSPORTATION`   INT(11) UNSIGNED           NULL, 
+`CAFEBAKERY`   INT(11) UNSIGNED           NULL, 
+`CONVENIENCE`   INT(11) UNSIGNED           NULL, 
+`AMUSEMENTPARK`   INT(11) UNSIGNED           NULL, 
+`MEDICAL`   INT(11) UNSIGNED           NULL
+);
 
